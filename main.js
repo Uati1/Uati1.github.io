ee619137ab01e426905b0dc79203041f97a28864
Szymon Łatacz <Szymon.Latacz@sage.com>
2020-07-22 15:35:17 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+let harr = [1000,3200,4050,5040,5950,7100,8400,9300,10500,11800,12550];
+let names = ['.header','.timmy','.bubba','.otto','.trippy','.ronny','.barnet','.aliz','.lula','.blake','.jay'];
+let n = 0;
+const positions = ()=>{
+    const width = $( window ).width()
+    const height = 14400*width/3840;
+    temp=[];
+    harr.forEach((height,index )=>{
+        temp[index]= height*width/3840;
+    });
+    $('.wrapper').height(height);
+    $('.fadein').height(height);
+    $('.white').height(height);
+    $('.colors').height(height);
+    harr= temp
+}
+
+$(window).scroll(function() {
+    var view = $(window).scrollTop()+ $( window ).height();
+    for(i=n;i<11;i++){
+      if(view>=harr[i]){
+        $(`${names[i]}`).css('opacity','1');
+        n+=1;
+      }
+    }
+  
+  });
+
+$( document ).ready(()=>{
+    positions();
+});
+
+$( window ).resize(()=>{
+    positions();
+});
+$(function() {
+    $('.toggler').click(function() {
+      $('.navbar').toggleClass('hideNav');
+      $('.bg-holder').toggleClass('hideNav');
+    });
+  });
+
+  (function($) { // Begin jQuery
+    $(function() { // DOM ready
+      // Toggle open and close nav styles on click
+      $('#nav-toggle').click(function() {
+        $('nav ul').slideToggle();
+        $('bg-holder').slideToggle();
+      });
+      // Hamburger to X toggle
+      $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active');
+      });
+    }); // end DOM ready
+  })(jQuery); // end jQuery
